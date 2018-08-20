c1f8a0db9d2814dfc8210f268d3142779ed628f0
Asaf Ebgi <asafebgi@gmail.com>
2018-08-20 07:39:33 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+// This example demonstrates decoding a JPEG image and examining its pixels.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
+	//_ "image/gif"
+	//_ "image/jpeg"
+)
+
+func main() {
+	reader, err := os.Open("./input/hex.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	//reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(data))
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bounds := m.Bounds()
+
+	//Split image into grid of 25x60 pixels
+	//Iterate through the elements setting a random color
+	const GRID_W = 25
+	const GRID_H = 60
+
+	out := image.NewRGBA(image.Rect(0, 0, bounds.Max.X, bounds.Max.Y))
+	//out.Pix[3] = 123
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			//r, g, b, a := m.At(x, y).RGBA()
+			if x%GRID_W == 0 && y%GRID_H == 0 {
+				//out.Pix[x*y] = 123
+				//fmt.Print(x, y)
+			}
+			out.Pix[x*y] = m.At(x, y)
+
+		}
+	}
+
+	outputFile, err := os.Create("./output/out.png")
+	if err != nil {
+	}
+	png.Encode(outputFile, out)
+	outputFile.Close()
+
+	fmt.Printf("test.")
+}
