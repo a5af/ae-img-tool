6e470c793be107bf399b83afcf971001417bf998
Asaf Ebgi <asafebgi@gmail.com>
2018-10-08 07:54:10 -0700

    addl

---
+++ draw.go
@@ -0,0 +1,15 @@
+// This example demonstrates decoding a JPEG image and examining its pixels.
+package main
+
+//_ "image/gif"
+//_ "image/jpeg"
+import (
+	"fmt"
+	"image"
+)
+
+func Draw() {
+	r := image.Rect(2, 1, 5, 5)
+	fmt.Print(r)
+
+}
