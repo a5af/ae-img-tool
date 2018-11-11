b4ae655b1dc1eb7ee73fe51532167909a4dc7b50
Asaf Ebgi <asafebgi@gmail.com>
2018-11-10 19:57:03 -0800

    checkered draw working

--- hex.go
+++ hex.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"log"
@@ -24,20 +25,25 @@ func hex() {
 
 	bounds := inputImage.Bounds()
 	outImage := image.NewRGBA(bounds)
-	//blue := color.RGBA{0, 0, 255, 100}
+	blue := color.RGBA{0, 0, 255, 100}
 
-	const WIDTH = 30
-	const HEIGHT = 60
+	const WIDTH = 5
+	const HEIGHT = 5
 
+	flag := true
 	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			if x%WIDTH == 0 && y%HEIGHT == 0 {
-				draw.Draw(outImage, image.Rect(x, y, WIDTH, HEIGHT), inputImage, image.ZP, draw.Src)
+				if flag {
+					draw.Draw(outImage, image.Rect(x, y, x+WIDTH, y+HEIGHT), inputImage, image.Point{x, y}, draw.Src)
+
+				} else {
+					draw.Draw(outImage, image.Rect(x, y, x+WIDTH, y+HEIGHT), &image.Uniform{blue}, image.ZP, draw.Src)
+				}
+				flag = !flag
 			}
 		}
 	}
-
-	//draw.Draw(outImage, outImage.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
 
 	// bounds := m.Bounds()
 
